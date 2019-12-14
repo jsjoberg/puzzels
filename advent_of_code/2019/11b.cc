2fdab660eab4963a095c81f5e30a61a5d784fa3e
Johan Sjoberg <jsjoberg@gmail.com>
2019-12-14 22:07:16 +0100

    AOC2019: Day 11 part 2, clean up

--- advent_of_code/2019/11b.cc
+++ advent_of_code/2019/11b.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <limits>
 #include <map>
 #include "src/computer.cc"
 
@@ -7,6 +6,8 @@ int main() {
 	computer c(load());
 	std::map<std::pair<int, int>, int> m;
 	int x = 0, y = 0, d = 0;
+	int x_min = 0, y_min = 0;
+	int x_max = 0, y_max = 0;
 	std::pair<int, int> o = { 0, 0 };
 	m[o] = 1;
 	c.run();
@@ -19,23 +20,16 @@ int main() {
 		d = (d + (c.get() ? 3 : 1)) % 4;
 		x = x + (d == 1 ? 1 : d == 3 ? -1 : 0);
 		y = y + (d == 0 ? 1 : d == 2 ? -1 : 0);
+		if (x > x_max) x_max = x;
+		if (x < x_min) x_min = x;
+		if (y > y_max) y_max = y;
+		if (y < y_min) y_min = y;
 		c.run();
-	}
-	int x_min, y_min;
-	int x_max, y_max;
-	x_min = y_min = std::numeric_limits<int>::max();
-	x_max = y_max = std::numeric_limits<int>::min();
-	for (auto kv : m) {
-		auto p = kv.first;
-		if (p.first > x_max) x_max = p.first;
-		if (p.first < x_min) x_min = p.first;
-		if (p.second > y_max) y_max = p.second;
-		if (p.second < y_min) y_min = p.second;
 	}
 	for (int i = y_max; i >= y_min; --i) {
 		for (int j = x_max; j >= x_min; --j) {
 			std::pair<int, int> p = { j, i };
-			std::cout << (m.count(p) ? (m[p] ? '#' : '.') : ' ');
+			std::cout << (!m.count(p) ? ' ' : m[p] ? '#' : '.');
 		}
 		std::cout << "\n";
 	}
