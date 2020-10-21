2831be34060aa2ce9f030024e6638a9174febb03
Johan Sjöberg <jsjoberg@gmail.com>
2020-10-21 22:57:03 +0200

    Project Euler: Problem 9

---
+++ project_euler/p9/p9.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	for a := 0; a <= 1000; a++ {
+		for b := a + 1; b <= 1000; b++ {
+			for c := b + 1; c <= 1000; c++ {
+				if a+b+c == 1000 && a*a+b*b == c*c {
+					fmt.Println(a * b * c)
+					return
+				}
+			}
+		}
+	}
+}
