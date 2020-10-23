e85b86ed6090b39623013313dac9ea45e4111649
Johan Sjöberg <jsjoberg@gmail.com>
2020-10-23 22:57:24 +0200

    Project Euler: Problem 9 speed optimization

--- project_euler/p9/p9.go
+++ project_euler/p9/p9.go
@@ -4,13 +4,16 @@ import "fmt"
 
 func main() {
 	for a := 0; a <= 1000; a++ {
-		for b := a + 1; b <= 1000; b++ {
-			for c := b + 1; c <= 1000; c++ {
-				if a+b+c == 1000 && a*a+b*b == c*c {
-					fmt.Println(a * b * c)
-					return
-				}
+		b := a + 1
+		c := 1000 - a - b
+		aa := a * a
+		for b < c {
+			if aa+b*b == c*c {
+				fmt.Println(a * b * c)
+				return
 			}
+			b++
+			c--
 		}
 	}
 }
