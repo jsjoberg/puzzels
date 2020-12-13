8a02d75df362df8cc87287f123e024c985e51a98
Johan Sjöberg <jsjoberg@gmail.com>
2020-12-13 02:25:05 +0100

    AOC2020 day 11 optimizations

--- advent_of_code/2020/day11.py
+++ advent_of_code/2020/day11.py
@@ -1,76 +1,91 @@
-def see(area, i, j):
-    mi = len(area)
-    mj = len(area[0])
-    dis = (-1, -1, -1,  0, 0,  1, 1, 1)
-    djs = (-1,  0,  1, -1, 1, -1, 0, 1)
-    n = 0
-    for di, dj in zip(dis, djs):
-        if (0 <= i+di < mi and
-            0 <= j+dj < mj and
-            area[i+di][j+dj] == 2):
-            n += 1
-    return n
+class Seat:
+    def __init__(self, limit):
+        self.neighbors = []
+        self.taken = False
+        self.change = False
+        self.limit = limit
 
-def see2(area, i, j):
-    mi = len(area)
-    mj = len(area[0])
-    dis = (-1, -1, -1,  0, 0,  1, 1, 1)
-    djs = (-1,  0,  1, -1, 1, -1, 0, 1)
-    n = 0
-    for di, dj in zip(dis, djs):
-        s = 1
-        while (0 <= i+di*s < mi and
-               0 <= j+dj*s < mj and
-               area[i+di*s][j+dj*s] == 0):
-            s += 1
-        if (0 <= i+di*s < mi and
-            0 <= j+dj*s < mj and
-            area[i+di*s][j+dj*s] == 2):
-            n += 1
-    return n
+    def add(self, s):
+        self.neighbors.append(s)
 
-def tick(area, area2, fn, n):
-    for i in range(len(area)):
-        for j in range(len(area[0])):
-            area2[i][j] = area[i][j]
-            if area[i][j] == 1:
-                if fn(area, i, j) == 0:
-                    area2[i][j] = 2
-            elif area[i][j] == 2:
-                if fn(area, i, j) >= n:
-                    area2[i][j] = 1
-    return area2, area
+    def tick(self):
+        if (not self.taken and
+            not any(s.taken for s in self.neighbors)):
+            self.change = True
+        if (self.taken and
+            sum(s.taken for s in self.neighbors) >= self.limit):
+            self.change = False
 
-def cmp(area, area2):
-    for a, b in zip(area, area2):
-        for i, j in zip(a, b):
-            if i != j:
-                return False
-    return True
+    def update(self):
+        diff = self.taken != self.change
+        self.taken = self.change
+        return diff
 
-def start(area, fn, x):
-    area2 = [a.copy() for a in area]
-    while True:
-        area, area2 = tick(area, area2, fn, x)
-        if cmp(area, area2):
-           break
-    n = 0
-    for a in area:
-        for x in a:
-            if x == 2:
-                n += 1
-    return n
+class Area:
+    def __init__(self, data):
+        xs = [[Seat(4) if c == "L" else None for c in l]
+              for l in data]
+        self.seats = []
+        mi = len(xs)
+        mj = len(xs[0])
+        dis = (-1, -1, -1,  0, 0,  1, 1, 1)
+        djs = (-1,  0,  1, -1, 1, -1, 0, 1)
+        for i in range(len(xs)):
+            for j in range(len(xs[0])):
+                if xs[i][j]:
+                    for di, dj in zip(dis, djs):
+                        if (0 <= i+di < mi and
+                            0 <= j+dj < mj and
+                            xs[i+di][j+dj]):
+                            xs[i][j].add(xs[i+di][j+dj])
+                    self.seats.append(xs[i][j])
 
-def parse(data):
-    return [[1 if c == "L" else 0 for c in l] for l in data]
+    def run(self):
+        while True:
+            for s in self.seats:
+                s.tick()
+            f = True
+            for s in self.seats:
+                f = not s.update() and f
+            if f:
+                break
+
+    def taken(self):
+        return sum(s.taken for s in self.seats)
+
+class Area2(Area):
+    def __init__(self, data):
+        xs = [[Seat(5) if c == "L" else None for c in l]
+              for l in data]
+        self.seats = []
+        mi = len(xs)
+        mj = len(xs[0])
+        dis = (-1, -1, -1,  0, 0,  1, 1, 1)
+        djs = (-1,  0,  1, -1, 1, -1, 0, 1)
+        for i in range(len(xs)):
+            for j in range(len(xs[0])):
+                if xs[i][j]:
+                    for di, dj in zip(dis, djs):
+                        s = 1
+                        while (0 <= i+di*s < mi and
+                            0 <= j+dj*s < mj and
+                            not xs[i+di*s][j+dj*s]):
+                            s += 1
+                        if (0 <= i+di*s < mi and
+                            0 <= j+dj*s < mj and
+                            xs[i+di*s][j+dj*s]):
+                            xs[i][j].add(xs[i+di*s][j+dj*s])
+                    self.seats.append(xs[i][j])
 
 def part1(data):
-    area = parse(data)
-    return start(area, see, 4)
+    area = Area(data)
+    area.run()
+    return area.taken()
 
 def part2(data):
-    area = parse(data)
-    return start(area, see2, 5)
+    area = Area2(data)
+    area.run()
+    return area.taken()
 
 if __name__ == "__main__":
     import sys
